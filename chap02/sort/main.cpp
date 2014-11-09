89e0854be8bf4a205537f0692a514522258b4bf9
tongzhipeng <tongzhipeng5688@gmail.com>
2014-11-09 22:26:05 +0800

    add files for sort project

---
+++ chap02/sort/main.cpp
@@ -0,0 +1,14 @@
+#include <QApplication>
+#include <QDialog>
+
+#include "sortdialog.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    SortDialog* dialog = new SortDialog;
+    dialog->setColumnRange('C', 'F');
+    dialog->show();
+    return app.exec();
+}
